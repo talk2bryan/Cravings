1cd03c84389bf3ae9a5855d34a93a3b8589bea9d
uju96 <hillary.nnamani@hotmail.com>
2016-11-09 22:29:59 -0600

    cravings First page
    First trial of the first page of the website

---
+++ script.js
@@ -0,0 +1,42 @@
+carousel = (function()
+{
+  if (!document.querySelector ||  !('classList' in document.body)) 
+   {
+    return false;
+  }
+  
+  var box = document.querySelector('.carouselbox');
+  var next = box.querySelector('.next');
+  var prev = box.querySelector('.prev');
+  
+  var counter = 0;
+  var items = box.querySelectorAll('.content li');
+  var amount = items.length;
+  var current = items[0];
+
+  box.classList.add('active');
+  
+  function navigate(direction)
+ {
+     current.classList.remove('current');
+    counter = counter + direction;
+      if (direction === -1 && 
+        counter < 0) 
+    { 
+      counter = amount - 1; 
+    }
+
+          if (direction === 1 &&  ! items[counter])
+   { 
+      counter = 0;
+   }
+    current = items[counter];
+    current.classList.add('current');
+ }
+  
+  next.addEventListener('click', function(ev) { navigate(1); } );
+  prev.addEventListener('click', function(ev) { navigate(-1); } );
+  navigate(0);
+}
+)
+();
