4f1eb31d6ce6b7c427ef2673d4fcf30b5804bda7
Bryan Wodi <talk2kamp@gmail.com>
2016-11-19 18:01:04 -0600

    javascript voided

--- milestones/Milestone3/js/behaviour.js
+++ milestones/Milestone3/js/behaviour.js
@@ -2,30 +2,46 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed!";
 }
-$(document).ready(function() {
+function test(){
+    var div = document.getElementById('restaurant-list-container');
 
+    div.innerHTML = div.innerHTML + 'Extra stuff';
+    var ok = "haha";
+    document.getElementsByClassName('body')[0].append(ok);
+}
+$(document).ready(function() {
+    var source = "../resources";
     // load row
     $.ajax({
-        type: "GET",
-        url: "../resources/data.csv",
-        success: function (data) {
+            type: "GET",
+            url: source +"/data.csv",
+            success: function (data) {
+                test();
+                var bufferString = Papa.unparse(Papa.parse(data).data);
+                var arr = bufferString.split('\n');
 
-            var bufferString = Papa.unparse(Papa.parse(data).data);
-            var arr = bufferString.split('\n');
-            var idName = document.getElementById('restaurant-list-container');
+                var restList = [];
+                var headers = arr[0].split(',');
+                for(var i = 1; i < arr.length; i++) {
+                    var row_data = arr[i].split(',');
+                    var obj = {};
+                    for(var j = 0; j < row_data.length; j++) {
+                        obj[headers[j].trim()] = row_data[j].trim();
+                    }
+                    restList.push(obj);
+                }
 
             var iDiv = document.createElement('div');
             iDiv.id = 'body';
             iDiv.className = 'block';
 
-            var restList = [];
-            // var headers = arr[0].split(',');
-            for (var i = 1; i < arr.length; i++) {
+            for (var ii = 1; ii< restList.length; i++) {
                 var innerDiv = document.createElement('div');//one restaurant row
                 innerDiv.className = 'restaurant-list';
 
-                var row = arr[i].split(',');
-                innerDiv.append($("<h4 />", row[0]));
+                var row = restList[ii];
+                // innerDiv.append($("<h4 />", text(row.name)));
+                innerDiv.appendData("Hello");
                 iDiv.appendChild(innerDiv);
                 // var obj = {};
                 // for (var j = 0; j < row.length; j++) {
@@ -33,8 +49,9 @@ $(document).ready(function() {
                 // }
                 // restList.push(obj);
             }
-            // idName.appendChild(innerDiv);
-            document.getElementById('restaurant-list-container').appendChild(iDiv);
+                document.getElementsByTagName('body')[0].appendChild(iDiv);            // idName.appendChild(innerDiv);
+
+            document.getElementById('body').appendChild(iDiv);
         }
     });
 });
