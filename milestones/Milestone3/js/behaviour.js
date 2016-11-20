3ae1167c169c3d347a6adac4b8eebb0606068b4c
Bryan Wodi <talk2kamp@gmail.com>
2016-11-20 13:06:11 -0600

    trying to fix image loading problem

--- milestones/Milestone3/js/behaviour.js
+++ milestones/Milestone3/js/behaviour.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
 
                 var restList = [];
                 var headers = arr[0].split(',');
+
                 for(var i = 1; i < arr.length; i++) {
                     var row_data = arr[i].split(',');
                     var obj = {};
@@ -30,75 +31,77 @@ $(document).ready(function() {
                 iDiv.className = 'block';
 
                 for (var ii = 0; ii<restList.length-1; ii++) {
-                var innerDiv = document.createElement('div');//one restaurant row
-                innerDiv.className = 'restaurant-list';
-                innerDiv.id = 'block-2';
 
-                var row = restList[ii];
+                    var innerDiv = document.createElement('div');//one restaurant row
+                    innerDiv.className = 'restaurant-list';
+                    innerDiv.id = 'block-2';
 
-                var aDiv = document.createElement('a');
-                aDiv.className = 'show-loading';
-                aDiv.setAttribute('href',source+"/Prototype.html"); //link to restaurant
 
-                var fullDiv = document.createElement('span');
-                fullDiv.className = 'full-div';
-                aDiv.appendChild(fullDiv);
+                    var row = restList[ii];
 
-                // logoDiv.setAttribute('href',source+"/Prototype.html");
+                    var aDiv = document.createElement('a');
+                    aDiv.className = 'show-loading';
+                    aDiv.setAttribute('href', source + "/Prototype.html"); //link to restaurant
 
-                var parentRow = document.createElement('div');
-                parentRow.className='row';
+                    var fullDiv = document.createElement('span');
+                    fullDiv.className = 'full-div';
+                    aDiv.appendChild(fullDiv);
 
-                var restLogo = document.createElement('div');
-                restLogo.className = 'restaurant-logo';
+                    // logoDiv.setAttribute('href',source+"/Prototype.html");
 
-                var innerLogoA = document.createElement('a');
-                innerLogoA.className = 'show-loading';
-                innerLogoA.setAttribute('href',source+"/Prototype.html");//link to restaurant
+                    var parentRow = document.createElement('div');
+                    parentRow.className = 'row';
 
-                var logoImg = document.createElement('img');
-                logoImg.className = 'logo-img';
-                console.log(row.picture);
+                    var restLogo = document.createElement('div');
+                    restLogo.className = 'restaurant-logo';
+
+                    var innerLogoA = document.createElement('a');
+                    innerLogoA.className = 'show-loading';
+                    innerLogoA.setAttribute('href', source + "/Prototype.html");//link to restaurant
+
+                    var logoImg = document.createElement('img');
+                    logoImg.className = 'logo-img';
+                    console.log(row.picture);
                     //bug here, TODO
                     //https://raw.githubusercontent.com/talk2bryan/Cravings/master/milestones/Milestone3/Prototype.html
                     //https://raw.githubusercontent.com/talk2bryan/Cravings/master/images/hseih.jpg
-                logoImg.setAttribute('src',row.picture);
+                    logoImg.setAttribute('src', row.picture);
                     //workaround
                     // logoImg.setAttribute('src',source+ '/resources/images'+row.picture);
 
-                innerLogoA.appendChild(logoImg);
-                restLogo.appendChild(innerLogoA);
+                    innerLogoA.appendChild(logoImg);
+                    restLogo.appendChild(innerLogoA);
 
-                var restInfoDiv = document.createElement('div');
-                restInfoDiv.className = 'col-xs-10';
+                    var restInfoDiv = document.createElement('div');
+                    restInfoDiv.className = 'col-xs-10';
 
-                var rowNoGuttersDiv = document.createElement('div');
-                rowNoGuttersDiv.className = 'row no-gutters';
+                    var rowNoGuttersDiv = document.createElement('div');
+                    rowNoGuttersDiv.className = 'row no-gutters';
 
-                var restInfoBucket = document.createElement('div');
-                restInfoBucket.className = 'col-xs-12 restaurant-name-content';
-                var restName = document.createElement('span');
-                restName.className = 'restaurant-name';
-                restName.textContent = row.name;
-                var restLoc = document.createElement('div');
-                restLoc.className = 'location-name';
-                restLoc.textContent = row.address + ', ' + row.city + ' ' + row.province + '. '+ row.postalCode;
-                restInfoBucket.appendChild(restName);
-                restInfoBucket.appendChild(restLoc);
+                    var restInfoBucket = document.createElement('div');
+                    restInfoBucket.className = 'col-xs-12 restaurant-name-content';
+                    var restName = document.createElement('span');
+                    restName.className = 'restaurant-name';
+                    restName.textContent = row.name;
+                    var restLoc = document.createElement('div');
+                    restLoc.className = 'location-name';
+                    restLoc.textContent = row.address + ', ' + row.city + ' ' + row.province + '. ' + row.postalCode;
+                    restInfoBucket.appendChild(restName);
+                    restInfoBucket.appendChild(restLoc);
 
-                rowNoGuttersDiv.appendChild(restInfoBucket);
-                restInfoDiv.appendChild(rowNoGuttersDiv);
+                    rowNoGuttersDiv.appendChild(restInfoBucket);
+                    restInfoDiv.appendChild(rowNoGuttersDiv);
 
-                parentRow.appendChild(restLogo);
-                parentRow.appendChild(restInfoDiv);
+                    parentRow.appendChild(restLogo);
+                    parentRow.appendChild(restInfoDiv);
 
-                //encapsulate everything as a row
-                innerDiv.appendChild(aDiv);
-                innerDiv.appendChild(parentRow);
+                    //encapsulate everything as a row
+                    innerDiv.appendChild(aDiv);
+                    innerDiv.appendChild(parentRow);
 
-                iDiv.appendChild(innerDiv); //add it to growing list of restaurants
+                    iDiv.appendChild(innerDiv); //add it to growing list of restaurants
+                }
+                document.getElementById('restaurant-list-container').appendChild(iDiv); //add it to container
             }
-            document.getElementById('restaurant-list-container').appendChild(iDiv); //add it to container
-        }
     });
 });
